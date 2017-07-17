182f66b7db45dcc9134ecbd2129c3491aad2778e
Camilo Aguero Botero <camiloab01@gmail.com>
2017-07-17 09:09:52 -0600

    Added files

---
+++ CuerpoActivoWPFClient/CommunicationHandler.cs
@@ -0,0 +1,70 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using Microsoft.AspNet.SignalR.Client;
+using SignalRBroadcastServiceSample.Domain;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CuerpoActivoWPFClient
+{
+    public class CommunicationHandler
+    {
+        private String baseUrl = "http://dev-cuerpoactivo.lumenup.net/";
+        private String attachment;
+
+        public CommunicationHandler()
+        {
+            InitiateCommunication();
+        }
+
+        private void InitiateCommunication()
+        {
+            string ip = String.Format("http://{0}:8084", ConfigurationManager.AppSettings.Get("IPAddress")); 
+            var hubConnection = new HubConnection(ip);
+            IHubProxy cuerpoActivoHubProxy = hubConnection.CreateHubProxy("CuerpoActivoServiceHub");
+
+            // This line is necessary to subscribe for broadcasting messages
+            cuerpoActivoHubProxy.On<Reminder>("NotifyReminder", HandleReminder);
+
+            // Start the connection
+            hubConnection.Start().Wait();
+        }
+
+        private void HandleReminder(Reminder _reminder)
+        {
+            Application.Current.Dispatcher.Invoke(new System.Action(() =>
+            {
+                TaskbarIcon notification = new TaskbarIcon();
+                String notificationMessage = string.Empty;
+                Icon icon = Properties.Resources.icon_colores;
+
+                if(!string.IsNullOrEmpty(_reminder.Reminder_attachment))
+                {
+                    attachment = baseUrl + _reminder.Reminder_attachment;
+                    notificationMessage = _reminder.Reminder_preview_description + Environment.NewLine + baseUrl + _reminder.Reminder_attachment;
+                    notification.TrayBalloonTipClicked += new RoutedEventHandler(notification_Click);
+                }
+                else
+                {
+                    notificationMessage = _reminder.Reminder_preview_description;
+                }
+                
+                notification.ShowBalloonTip(_reminder.Reminder_title, notificationMessage, icon, false);
+            }));
+        }
+
+        private void notification_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(attachment);
+        }
+    }
+}
---
+++ CuerpoActivoWindowsClient/CommunicationHandler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using SignalRBroadcastServiceSample.Domain;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsClient
+{
+    public class CommunicationHandler : ApplicationContext
+    {
+        public CommunicationHandler()
+        {
+            InitiateCommunication();
+        }
+
+        private static void InitiateCommunication()
+        {
+            var hubConnection = new HubConnection("http://localhost:8084");
+            IHubProxy cuerpoActivoHubProxy = hubConnection.CreateHubProxy("CuerpoActivoServiceHub");
+
+            // This line is necessary to subscribe for broadcasting messages
+            cuerpoActivoHubProxy.On<Reminder>("NotifyReminder", HandleReminder);
+
+            // Start the connection
+            hubConnection.Start().Wait();
+        }
+
+        private static void HandleReminder(Reminder reminder)
+        {
+            var item = new NotifyIcon();
+            item.Visible = true;
+            item.Icon = SystemIcons.Information;
+            item.ShowBalloonTip(3000, reminder.Reminder_title, reminder.Reminder_preview_description, ToolTipIcon.Info);
+        }
+    }
+}
